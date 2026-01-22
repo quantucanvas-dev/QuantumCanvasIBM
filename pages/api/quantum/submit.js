9396bad0b4d6b0afe0bd018f5e7a780b2c70b32e
Quantu <quantumordinals@gmail.com>
2026-01-22 11:56:56 +0400

    Clean up comments in submit.js
    Removed comments related to quantum circuit creation and job submission.

--- pages/api/quantum/submit.js
+++ pages/api/quantum/submit.js
@@ -1,6 +1,3 @@
-// pages/api/quantum/submit.js
-// IBM Quantum Job Submission API Route
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
@@ -17,10 +14,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    // Create quantum circuit in OpenQASM format
     const circuit = createQuantumCircuit(qubits, style);
     
-    // Submit to IBM Quantum Runtime API
     const response = await fetch('https://api.quantum-computing.ibm.com/runtime/jobs', {
       method: 'POST',
       headers: {
@@ -61,7 +56,6 @@ export default async function handler(req, res) {
   }
 }
 
-// Helper: Create quantum circuit in OpenQASM format
 function createQuantumCircuit(qubits, style) {
   let qasm = `OPENQASM 2.0;
 include "qelib1.inc";
@@ -70,13 +64,11 @@ creg c[${qubits}];
 `;
 
   if (style === 'superposition' || !style) {
-    // Apply Hadamard gates for equal superposition
     for (let i = 0; i < qubits; i++) {
       qasm += `h q[${i}];
 `;
     }
   } else if (style === 'entangled') {
-    // Create Bell pairs (maximum entanglement)
     for (let i = 0; i < qubits - 1; i += 2) {
       qasm += `h q[${i}];
 `;
@@ -84,7 +76,6 @@ creg c[${qubits}];
 `;
     }
   } else if (style === 'interference') {
-    // Quantum interference pattern
     for (let i = 0; i < qubits; i++) {
       qasm += `h q[${i}];
 `;
@@ -97,7 +88,6 @@ creg c[${qubits}];
     }
   }
 
-  // Measure all qubits
   for (let i = 0; i < qubits; i++) {
     qasm += `measure q[${i}] -> c[${i}];
 `;
