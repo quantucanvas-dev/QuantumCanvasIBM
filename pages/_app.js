41ea3a17fff53b6cb41123ed7b423192fbb4f06b
Quantu <quantumordinals@gmail.com>
2026-01-22 11:55:41 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
